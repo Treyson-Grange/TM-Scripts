8627e4649c243b5174bf2a6744b21801b9ab2271
Treyson <treyson.grange@outlook.com>
2024-10-31 17:02:16 -0600

    new proj

---
+++ GH-Halloween/script.js
@@ -0,0 +1,12 @@
+// ##bd561d
+// ##fa7a18
+// ##631c03
+// ##fddf68
+
+//TODO
+// ALL of it.
+
+// General IDEA
+// GH on halloween, had a really cool theme for the contributions graph. I want it all the time.
+// Maybe more useable, just set any theme I want.
+// Possibilities are endless✨
